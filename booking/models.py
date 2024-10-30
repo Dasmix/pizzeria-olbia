e0b790b3ec879efc8c109aa2cc75c50d486057e9
Damian Kolacki <dasmixovski@gmail.com>
2024-10-30 19:30:42 +0000

    Create reservation model with guest, day, time, user and contact details

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+TIME_CHOICES = (
+    ('5:30 PM', '5:30 PM'),
+    ('6:00 PM', '6:00 PM'),
+    ('6:30 PM', '6:30 PM'),
+    ('7:00 PM', '7:00 PM'),
+    ('7:30 PM', '7:30 PM'),
+    ('8:00 PM', '8:00 PM'),
+    ('8:30 PM', '8:30 PM'),
+    ('9:00 PM', '9:00 PM'),
+    ('9:30 PM', '9:30 PM'),
+    ('10:00 PM', '10:00 PM')
+)
+
+GUEST_CHOICES = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+)
+
+class Reservation (models.Model):
+    guest = models.CharField(max_length=10, choices=GUEST_CHOICES)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES)
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100, default="")
+    email = models.EmailField(max_length=100, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             null=True, blank=True)
--- pizzeria_olbia/settings.py
+++ pizzeria_olbia/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@js&b25tfto#+hk7fmii_7et)_3elh098dsrs9_-tscf)^l(9='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dasmix-pizzeriaolbia-7avu3onc5o0.ws-eu116.gitpod.io', 'localhost','.herokuapp.com',]
 
