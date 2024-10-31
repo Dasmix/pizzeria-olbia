9dcd9e0acff60f7c69304f391c91f9d7da48210b
Damian Kolacki <dasmixovski@gmail.com>
2024-10-31 08:34:34 +0000

    fix booking html files and add login html files

--- booking/urls.py
+++ booking/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('book_table/', views.book_table, name='book_table'),
     path('manage_reservation/', views.manage_reservation, name='manage_reservation'),
-        path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
