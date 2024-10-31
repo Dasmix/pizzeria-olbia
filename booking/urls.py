56f08fe392124bf14e8bf1feea25449b6ab1a18f
Damian Kolacki <dasmixovski@gmail.com>
2024-10-31 08:11:21 +0000

    Add login and logout buttons and theirs urls

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('book_table/', views.book_table, name='book_table'),
     path('manage_reservation/', views.manage_reservation, name='manage_reservation'),
+        path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
