2fabc461627b4c8c7c652d511890018b619a2835
Damian Kolacki <dasmixovski@gmail.com>
2024-10-31 01:29:19 +0000

    Add index.html to templates and urls.py

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Reservation
 # Create your views here.
+
+
+class HomeView(generic.ListView):
+    model = Reservation
+    template_name = 'booking/index.html'
--- pizzeria_olbia/settings.py
+++ pizzeria_olbia/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-@js&b25tfto#+hk7fmii_7et)_3elh098dsrs9_-tscf)^l(9=")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dasmix-pizzeriaolbia-7avu3onc5o0.ws-eu116.gitpod.io', 'localhost','.herokuapp.com',]
 
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pizzeria_olbia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'pizzeria_olbia.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://u81qu5j89lt:VqhWdDjFtGfU@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/front_view_level_601796"))
 }
 
 CSRF_TRUSTED_ORIGINS = [
