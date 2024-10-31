190ad03da7f08f87df9b0c5bb7cc93b0fe36f603
Damian Kolacki <dasmixovski@gmail.com>
2024-10-31 06:35:55 +0000

    Add 3 html files for book, manage, and register and urls.py / views.py are updated

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('register/', views.register, name='register'),
+    path('book_table/', views.book_table, name='book_table'),
+    path('manage_reservation/', views.manage_reservation, name='manage_reservation'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -7,3 +7,12 @@ from .models import Reservation
 class HomeView(generic.ListView):
     model = Reservation
     template_name = 'booking/index.html'
+
+def register(request):
+    return render(request, 'booking/register.html')
+
+def book_table(request):
+    return render(request, 'booking/book_table.html')
+
+def manage_reservation(request):
+    return render(request, 'booking/manage_reservation.html')
